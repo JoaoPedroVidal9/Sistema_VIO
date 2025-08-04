d1121ef90ab4565dec7e82feba387f93c459213d
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2025-08-04 16:17:20 -0300

    Adição de imagens

--- mysql-init/Events/arquivarcompras.sql
+++
@@ -1,9 +0,0 @@
-CREATE EVENT IF NOT EXISTS arquivar_compras_antigas
-    ON SCHEDULE EVERY 1 hour
-    STARTS CURRENT_TIMESTAMP + INTERVAL 5 MINUTE
-    ON COMPLETION PRESERVE
-    ENABLE
-DO 
-    INSERT INTO historico_compra (id_compra, data_compra, id_usuario) 
-    SELECT id_compra, data_compra, fk_id_usuario from compra
-    WHERE data_compra < NOW() - interval 6 MONTH;
--- mysql-init/Events/excluir_eventos.sql
+++
@@ -1,7 +0,0 @@
-CREATE EVENT IF NOT EXISTS excluir_eventos_antigos
-    ON schedule every 1 WEEK
-    STARTS CURRENT_TIMESTAMP + INTERVAL 5 MINUTE
-    ON COMPLETION PRESERVE
-    ENABLE
-DO
-    DELETE FROM evento WHERE data_hora < now() - interval 1 year;
