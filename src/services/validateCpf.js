e8f020e6c33ae55f7adc8d251bc72c67fac8cfab
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2025-08-06 14:07:06 -0300

    validando cpf matematicamente

--- src/services/validateCpf.js
+++ src/services/validateCpf.js
@@ -5,6 +5,9 @@ module.exports = async function validateCpf(cpf, userId = null) {
     const query = "SELECT id_usuario FROM usuario WHERE cpf = ?";
     const values = [cpf];
 
+    if(!validateCPFMath(cpf)){
+      resolve({ error:"Informe um CPF válido."})
+    }
     connect.query(query, values, (err, results) => {
       if (err) {
         reject("Erro ao verificar CPF");
@@ -25,4 +28,30 @@ module.exports = async function validateCpf(cpf, userId = null) {
     });
   });
 };
+
+function validateCPFMath(cpf){
+  // Remove caracteres não numéricos
+  cpf.replace(/[^\d]+/g,'');
+
+  // Verifica se há 11 digitos e se são digitos aceitos
+  if(cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+  //
+  const calcDigit = (base, pesoInicial) => {
+    let soma = 0;
+    for (let i = 0; i < base.length; i++){
+      soma += parseInt(base[i]) * (pesoInicial - i);
+    }
+    const resto = soma % 11;
+    return resto < 2 ? 0 : 11 - resto;
+  }
+  
+  const primeiroDigito = calcDigit(cpf.substring(0, 9), 10);
+  const segundoDigito = calcDigit(cpf.substring(0, 9) + primeiroDigito, 11);
+
+  return (
+    parseInt(cpf[9]) === primeiroDigito &&
+    parseInt(cpf[10]) === segundoDigito
+  );
+}
 
