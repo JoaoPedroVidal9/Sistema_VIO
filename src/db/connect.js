692a9b38ef1c31030c77668daf4039ebeca45f84
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2025-09-29 13:14:25 -0300

    Implementação com o railway

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,12 +1,16 @@
-// require('dotenv').config();
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    connectionLimit:10,
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+  connectionLimit: 10,
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+
+  password: process.env.MYSQLDATABASE || process.env.DB_NAME,
+
+  database: process.env.MYSQLPORT || 3306,
 });
 
 module.exports = pool;
